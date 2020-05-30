9528f6dc01cdf0701561dd3c89781c52ed770d10
xiaoyang <1483863489@qq.com>
2020-05-30 21:31:43 +0800

    update KafkaConsumerApp

--- src/main/java/com/xiaoyang/kafka/KafkaConsumerApp.java
+++ src/main/java/com/xiaoyang/kafka/KafkaConsumerApp.java
@@ -1,7 +1,10 @@
 package com.xiaoyang.kafka;
 
 import java.time.Duration;
+
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
@@ -11,12 +14,14 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
  * @author xiaoyang
  */
+@Slf4j
 public class KafkaConsumerApp {
     public final static String BROKER_LIST = "192.168.146.151:9092,192.168.146.152:9092,192.168.146.153:9092";
     public final static String TOPIC = "kafka-test";
@@ -31,19 +36,36 @@ public class KafkaConsumerApp {
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
 
         config.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-
+        //关闭自动提交,关闭自动提交后需要手动提交
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,false);
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
         //订阅主题
         consumer.subscribe(Collections.singletonList(TOPIC));
         //通过正则表达式订阅以kafka-开头的主题
         //consumer.subscribe(Pattern.compile("kafka-*"));
         //指定分区消费,
-        consumer.assign(Collections.singletonList(new TopicPartition(TOPIC,0)));
-
-        while(true){
-
+        //consumer.assign(Collections.singletonList(new TopicPartition(TOPIC,0)));
+        //consumer自动提交的消费形式。
+        /*while(true){
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
             records.forEach(record -> System.out.println(record.value()));
+        }*/
+        //手动提交
+        while(true){
+            ConsumerRecords<String, String> poll = consumer.poll(Duration.ofMillis(1000));
+            if (poll.isEmpty()){
+                break;
+            }
+            Iterable<ConsumerRecord<String, String>>  records = poll.records(TOPIC);
+            records.forEach(record-> {
+                log.info("手动提交消费：key-{},value-{}", record.key(), record.value());
+                log.info("offset:{},partition:{}",record.offset(),record.partition());
+            });
+
+            //同步提交
+            consumer.commitSync();
+            //异步提交
+            //consumer.commitAsync();
         }
     }
 

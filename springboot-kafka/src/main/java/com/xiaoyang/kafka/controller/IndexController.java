4b8fd744865a4876e46919faf22db1470b0b3c10
xiaoyang <1483863489@qq.com>
2020-06-21 16:06:46 +0800

    spring kafka 支持事务的两种方式

--- springboot-kafka/src/main/java/com/xiaoyang/kafka/controller/IndexController.java
+++ springboot-kafka/src/main/java/com/xiaoyang/kafka/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +26,26 @@ public class IndexController {
 
     @GetMapping("send/{msg}")
     public String send(@PathVariable String msg){
-        kafkaTemplate.send(TOPIC,msg);
-        return "suc";
+        //kafkaTemplate.send(TOPIC,msg);
+        //kafka 事务
+        kafkaTemplate.executeInTransaction(t->{
+            t.send(TOPIC,msg);
+            if ("error".equals(msg)) {
+                throw new RuntimeException("Error ");
+            }
+            return true;
+        });
+        return "success";
     }
-
+    @GetMapping("sendt/{msg}")
+    @Transactional(rollbackFor = Exception.class)
+    public String sendTr(@PathVariable String msg){
+        kafkaTemplate.send(TOPIC,msg);
+        if ("error".equals(msg)) {
+            throw new RuntimeException("Error ");
+        }
+        return "success";
+    }
     @KafkaListener(topics = TOPIC)
     public void listen(ConsumerRecord<?, ?> cr) throws Exception {
         log.info(cr.toString());
